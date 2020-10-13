c8fc035399d9b9dcff1670b8f234ec6ddfaff562
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-10-13 18:07:49 +0530

    added post end point to send info to backend

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
+
+
 //Node.js Everywhere with Environment Variables!
 //https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
-
 dotenv.config();
 //console.log(process.env.DB_USERNAME);
 
@@ -17,10 +20,29 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
 
 
 //https://expressjs.com/en/guide/routing.html
-
 app.get('/', (req, res) => {
-    res.send('Hello World !');
+    res.send('Hello World !!!');
 });
+
+
+//body parser
+//Express JS— body-parser and why may not need it
+//https://medium.com/@mmajdanski/express-body-parser-and-why-may-not-need-it-335803cd048c
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookie parser
+app.use(cookieParser());
+
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if (err) return err.json({ success: false, err });
+    })
+    return res.status(200);
+})
 
 
 //“Hello World!” app with Node.js and Express
