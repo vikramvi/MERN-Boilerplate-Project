4559f41c8c2eeae2b3677e60b4b526643a06adf5
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-10-16 13:55:37 +0530

    created client related boilerplate code

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      Concurrent testing
+      { /* <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} /> 
+      </Switch> */ }
+    </div >
+  );
+}
+
+export default App;
